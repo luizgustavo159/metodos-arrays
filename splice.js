67b1802e5b30bea1a1e10971f5a48f3691987a89
Luiz <luizgustavo159753@gmail.com>
2023-04-26 13:46:03 -0400

    finalizado

---
+++ splice.js
@@ -0,0 +1,6 @@
+let array = ["a", "b", "c", "d", "e"];
+// primeiro numero equivale a qual o indice vai ser mexido
+// segundo numero equivale a quantos indices vao ser excluidos
+// terceiro numero equivale a qual vai ser a substituiçao feita
+array.splice(1,2, "f");
+console.log(array);
